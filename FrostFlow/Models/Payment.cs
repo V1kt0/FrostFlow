7d50422480857411f0bb9f1ff30d108c16deee17
timo067 <aliosman.aliosmann@gmail.com>
2025-04-13 23:07:01 +0300

    added shopping car

---
+++ FrostFlow/Models/Payment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrostFlow.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+        public Order Order { get; set; } // Navigation property
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime PaymentDate { get; set; } = DateTime.Now;
+
+        public string PaymentMethod { get; set; } // Optional: e.g., "Credit Card", "PayPal"
+    }
+}
